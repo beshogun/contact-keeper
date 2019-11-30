3edc217e557bfb68b06077b3047047996276336a
beshogun <ben.roy@omnitude.tech>
2019-11-30 20:54:33 +0000

    removed initial rout

--- server.js
+++ server.js
@@ -1,31 +1,27 @@
-const express = require("express");
-const connectDB = require("./config/db");
-const path = require("path");
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
-// connect database
+// Connect Database
 connectDB();
 
-// init middleware
+// Init Middleware
 app.use(express.json({ extended: false }));
 
-// app.get("/", (req, res) =>
-//   res.json({ msg: "Welcome to the conact keeper api" })
-// );
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
-// define routes
-app.use("/api/users", require("./routes/users"));
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/contacts", require("./routes/contacts"));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-// serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  // set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
 
